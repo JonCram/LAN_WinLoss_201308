0e6fc44d9774ae16699d8b70645ccc7bd37df14d
cramcode <cramcode@gmail.com>
2014-06-21 17:04:57 -0600

    final barplot

--- LAN_WinLoss.R
+++ LAN_WinLoss.R
@@ -1,17 +1,23 @@
-library("ggplot2")
-library("grid")
-source("~/_dev/themes/theme_cram/theme_cram.R")
-df <- read.csv("~/_dev/tmp/results.csv")
-names(df) <-  c("date", "year", "score") # column names
-
-df <- df[grep("1966-08", df$date),] # date range; like yyyy-mm or yyyy
-
-df <- data.frame(df, df$date, dif = as.numeric(as.character(df$score))) # cast as numeric
-
-dat1 <- subset(df,df$dif >= 0) # grab winning diff
-dat2 <- subset(df,df$dif < 0) # grab losing diff
-
-ggplot() + 
-  geom_bar(data = dat1, aes(x=date, y=dif),stat = "identity") +
-  geom_bar(data = dat2, aes(x=date, y=dif),stat = "identity") +
-  theme_cram()
+  library("ggplot2")
+  library("grid")
+  source("~/_dev/themes/theme_cram/theme_cram.R")
+  df <- read.csv("~/_dev/tmp/results.csv")
+  names(df) <-  c("date", "year", "score") # column names
+  
+  df <- df[grep("1966", df$date),] # date range; like yyyy-mm or yyyy
+  
+  df <- data.frame(df, df$date
+                   , dif = as.numeric(as.character(df$score))) # cast as numeric
+  dat1 <- subset(df,df$dif >= 0) # grab winning diff
+  dat2 <- subset(df,df$dif < 0) # grab losing diff
+  
+  ggplot() + 
+    geom_bar(data = dat1, aes(x=date, y=dif),stat = "identity"
+             , fill="#005596", width=.9) +
+    geom_bar(data = dat2, aes(x=date, y=dif),stat = "identity"
+             , fill="#A31410", width=.9) +
+    theme_cram() +
+    theme(
+      axis.text.x = element_text(size=8, angle=90,hjust=.5,vjust=.5)
+    )
+  
